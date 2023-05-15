fbf01196996d7c91ef5509797d10eba3e7ca7428
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-05-15 18:48:00 +0200

    feat: Adds a batching mechanism

--- my_lib/users.py
+++ my_lib/users.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
-__all__ = ["greet_contributor"]
+__all__ = ["greet_contributor", "batch_user_messages"]
 
 
 def greet_contributor(name: str) -> str:
@@ -15,4 +15,19 @@ def greet_contributor(name: str) -> str:
     Returns:
         the greeting message
     """
-    return f"Hello {name}! Nice to meet you."
+    return "Hello " + name + "! Nice to meet you."
+
+
+def batch_user_messages(names):
+	"""Batches the log messages together
+
+    Args:
+        names: list of persons whose logs need to be processed
+
+    Returns:
+        the concatenated string
+    """
+
+    raw_batch = "\n".join([greet_contributor(name) for name in names])
+
+    return raw_batch.lower()
